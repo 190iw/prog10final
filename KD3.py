5a3b69272d267313f5a71abb2d1ee5d10c58c327
Ploons <159978962+Ploons@users.noreply.github.com>
2024-05-01 19:39:23 +0800

    Update KD3.py

---
+++ KD3.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+print ("The man outside was never your friend.")
+time.sleep(3)
+os.system('cls')
+print ("here's a reason why people are less scared of bears.")
+time.sleep(3)
+print ("At least bears belonged to the forest")
+time.sleep(3)
+os.system('cls')
+print ("It did not")
+time.sleep(3)
+os.system('cls')
+print ("He prowls closer and it's pitiful how you've forgotten to lock the door.")
