6f1fff6e3aa29dd7b4bcc1be0ad49dc5118ae441
Ploons <159978962+Ploons@users.noreply.github.com>
2024-05-01 19:42:43 +0800

    Update PbD3.py

---
+++ PbD3.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+print ("The man outside was never your friend.")
+time.sleep(3)
+print ("Fame doesn't always bring fortune,")
+time.sleep(3)
+os.system('cls')
+print ("it has brought a stalker.")
+time.sleep(3)
+os.system('cls')
+print ("He prowls closer and it's pitiful how you've forgotten to lock the door.")
