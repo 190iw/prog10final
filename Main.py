71282c9e01b2a4fbd8e80e3f44e9ee6388893034
190iw <137978904+190iw@users.noreply.github.com>
2024-05-02 12:27:35 +0800

    Update Main.py
    tkinter

--- Main.py
+++ Main.py
@@ -1,17 +1,13 @@
 import PainterD1
 import time
 import os
+import tkinter as tk
 
-time.sleep (2)
-input = str(input("Moments after I had ")) #Published or Kept in attic
-if input == ("Published"):
-    os.system('cls')
-    import PbD1
-elif input == ("Kept it in my attic"):
-    os.system('cls')
-    import KD1
-time.sleep(4)
-print ('''
+## VARIABLES
+
+published = 0
+kept = 0
+day2 = ('''
 ░▒▓███████▓▒░   ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░  
 ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░              ░▒▓█▓▒░ 
 ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░              ░▒▓█▓▒░ 
@@ -20,19 +16,159 @@ print ('''
 ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░           ░▒▓█▓▒░        
 ░▒▓███████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░    ░▒▓█▓▒░           ░▒▓████████▓▒░ 
                                                                   
-                                                                  
-''')
+                                                                  ''')
+
+## TKINTER
+def first_choices(choices):
+    choice_window = tk.Toplevel(root)
+    for choice in choices:
+        choice_button = tk.Button(choice_window, text=choice, command=lambda c=choice: handle_choice(c),
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="lightgray",
+                   cursor="hand2",
+                   disabledforeground="gray",
+                   fg="black",
+                   font=("Arial", 12),
+                   height=2,
+                   highlightbackground="black",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=15,
+                   wraplength=100)
+        choice_button.pack()
+
+    
+# Create the root window but keep it hidden
+root = tk.Tk()
+root.withdraw()
+
+def close_window():
+    root.quit() # destroys window yaay
+
+# opens window/starts loop
+def open_window():
+    root.mainloop()
+
+def second_choices(things):
+    choice_window = tk.Toplevel(root)
+    for choice in things:
+        choice_button = tk.Button(choice_window, text=choice, command=lambda c=choice: second_handle(c),
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="lightgray",
+                   cursor="hand2",
+                   disabledforeground="gray",
+                   fg="black",
+                   font=("Arial", 12),
+                   height=2,
+                   highlightbackground="black",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=15,
+                   wraplength=100)
+        choice_button.pack()
+
+def third_choices(things):
+    choice_window = tk.Toplevel(root)
+    for choice in things:
+        choice_button = tk.Button(choice_window, text=choice, command=lambda c=choice: third_handle(c),
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="lightgray",
+                   cursor="hand2",
+                   disabledforeground="gray",
+                   fg="black",
+                   font=("Arial", 12),
+                   height=2,
+                   highlightbackground="black",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=15,
+                   wraplength=100)
+        choice_button.pack()
+
+def second_handle(choice):
+    print("Selected:", choice)
+    if choice == "Fight":
+        close_window()
+        os.system('cls')
+        print ("You run down the hallway in a fit of screams.")
+        time.sleep(3)
+        print ("It must’ve scared the intruder and forced them to run away.")
+    elif choice == "Run Away":
+        close_window()
+        os.system('cls')
+        print("You can't run away.")
+        os.system('cls')
+        time.sleep(1)
+        print ("You run down the hallway in a fit of screams.")
+        time.sleep(3)
+        print ("It must’ve scared the intruder and forced them to run away.")
+
+def third_handle(choice):
+    print("Selected:", choice)
+    if choice == "RUN AFTER IT":
+        os.system('cls')
+        print ("You run out the house,")
+        time.sleep(3)
+        print ("waving your arms wildly over your head.")
+        time.sleep(3)
+        print ("You feel the tip of the knife a few times and the feeling of blood,")
+        time.sleep(3)
+        print ("trickling down your arm.")
+        
+        
+def handle_choice(choice):
+    print("Selected:", choice)
+    if choice == ("Published"):
+        global published
+        published += 1
+        close_window()
+        os.system('cls')
+        import PbD1
+    elif choice == ("Kept it in my attic"):
+        global kept 
+        kept += 1
+        close_window()
+        os.system('cls')
+        import KD1
+
+print("Moments after I had...")
+first_choices(["Published", "Kept it in my attic"])
+open_window()
+
+time.sleep(4)
+print(day2)
 time.sleep(3)
 import PainterD2
 
-input2 = str(input("I decided to "))
-if input2 == ("Publish","publish"):
+if published == 1:
     os.system('cls')
     import PbD2
-elif input2 == ("Keep","keep"):
+elif kept == 1:
     os.system('cls')
     import KD2
-    
+
+time.sleep(2) 
 os.system('cls')
 print ("I didn't eat today.")
 time.sleep(3)
@@ -76,11 +212,9 @@ time.sleep(5)
 os.system('cls')
 import PainterDZ
 
-input3 = str(input("DECIDE "))
-if input3 == ("Publish"):
-    os.system('cls')
+if published == 1:
     import PbD3
-elif input3 == ("Keep"):
+elif kept == 1:
     os.system('cls')
     import KD3
 
@@ -88,21 +222,15 @@ time.sleep(5)
 os.system('cls')
 import PainterDY
 
-input = str(input("You decide to "))
-if input == ("Fight","fight"):
-    os.system('cls')
-    print ("You run down the hallway in a fit of screams.")
-    time.sleep(3)
-    print ("It must’ve scared the intruder and forced them to run away.")
-elif input == ("Run after it","run after it"):
-    os.system('cls')
-    print ("You run out the house,")
-    time.sleep(3)
-    print ("waving your arms wildly over your head.")
-    time.sleep(3)
-    print ("You feel the tip of the knife a few times and the feeling of blood,")
-    time.sleep(3)
-    print ("trickling down your arm.")
+print("You decide to...")
+second_choices(["Fight", "Run Away"])
+open_window()
+
+time.sleep(2)
+open_window()
+print("Quick, It can't escape.")
+third_choices(["RUN AFTER IT"])
+open_window()
 
 time.sleep(3)
 os.system('cls')
